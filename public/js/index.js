82eb8411554bdb00e31550e306a8edbb6feb96cd
drewmw5 <66263013+drewmw5@users.noreply.github.com>
2020-10-30 01:07:33 -0700

    renamed added functionality to display a modal with a larger sized image

---
+++ public/js/index.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    viewFullImage();
+})
+
+function viewFullImage(){
+    $(".js-enlarge").click(function(e) {
+        e.preventDefault()
+        var image = $(this).attr('src');
+        var title = $(this).attr('alt');
+
+        $('.modal-title').html(title)
+        $('#modal-img').attr('src', image)
+        $('#modal').modal('show')
+    })
+}
