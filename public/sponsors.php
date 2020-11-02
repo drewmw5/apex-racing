0e23104601c0f6f4c170ce5cf56a1c20f66cddda
drewmw5 <66263013+drewmw5@users.noreply.github.com>
2020-11-01 22:58:50 -0800

    added footer

--- public/sponsors.php
+++ public/sponsors.php
@@ -2,6 +2,7 @@
 require_once 'includes/html/head.html';
 require_once 'includes/html/navbar.html';
 require_once 'includes/html/sponsors.html';
+require_once 'includes/html/footer.html';
 ?>
 </body>
 </html>
