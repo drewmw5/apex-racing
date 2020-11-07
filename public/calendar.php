248fb7245a32d29bb9b47da0020dfaff7198c2c1
drewmw5 <66263013+drewmw5@users.noreply.github.com>
2020-11-06 16:42:36 -0800

    init

---
+++ public/calendar.php
@@ -0,0 +1,12 @@
+<?php
+$title = " - Calendar";
+$desc = '"Welcome to the Apex Tech Racing Calendar Page! Here is where you can find when and where we\'re racing."';
+$canonical = '"https://www.apextechracing.com/gallery"';
+
+require_once 'includes/html/cal-head.php';
+require_once 'includes/html/navbar.html';
+require_once 'includes/html/calendar.html';
+require_once 'includes/html/footer.html';
+?>
+</body>
+</html>
---
+++ public/includes/html/cal-head.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content=<?php echo $desc; ?>>
+
+    <script defer src="js/jquery-3.5.1.min.js"></script>
+    <script defer src="js/bootstrap.min.js"></script>
+    <script defer src="js/calendar.min.js"></script>
+    <script defer src="js/index.min.js"></script>
+
+    <!-- Font Awesome JS -->
+    <script defer src="js/all.min.js"></script> <!--load all styles -->
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/calendar.min.css">
+    <link rel="stylesheet" href="css/styles.css">
+
+    <link rel="icon" href="images/Apex-A-symbol (1).jpg">
+
+    <link rel="canonical" href=<?php echo $canonical; ?> />
+
+    <title>Apex Tech Racing<?php echo $title;?></title>
+
+    <script>
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var calendarEl = document.getElementById('calendar');
+
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+      headerToolbar: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'dayGridMonth,timeGridWeek,timeGridDay'
+      },
+      initialDate: '2020-09-12',
+      navLinks: true, // can click day/week names to navigate views
+      selectable: true,
+      selectMirror: true,
+      select: function(arg) {
+        var title = prompt('Event Title:');
+        if (title) {
+          calendar.addEvent({
+            title: title,
+            start: arg.start,
+            end: arg.end,
+            allDay: arg.allDay
+          })
+        }
+        calendar.unselect()
+      },
+      eventClick: function(arg) {
+        if (confirm('Are you sure you want to delete this event?')) {
+          arg.event.remove()
+        }
+      },
+      editable: true,
+      dayMaxEvents: true, // allow "more" link when too many events
+      events: [
+        {
+          title: 'All Day Event',
+          start: '2020-09-01'
+        },
+        {
+          title: 'Long Event',
+          start: '2020-09-07',
+          end: '2020-09-10'
+        },
+        {
+          groupId: 999,
+          title: 'Repeating Event',
+          start: '2020-09-09T16:00:00'
+        },
+        {
+          groupId: 999,
+          title: 'Repeating Event',
+          start: '2020-09-16T16:00:00'
+        },
+        {
+          title: 'Conference',
+          start: '2020-09-11',
+          end: '2020-09-13'
+        },
+        {
+          title: 'Meeting',
+          start: '2020-09-12T10:30:00',
+          end: '2020-09-12T12:30:00'
+        },
+        {
+          title: 'Lunch',
+          start: '2020-09-12T12:00:00'
+        },
+        {
+          title: 'Meeting',
+          start: '2020-09-12T14:30:00'
+        },
+        {
+          title: 'Happy Hour',
+          start: '2020-09-12T17:30:00'
+        },
+        {
+          title: 'Dinner',
+          start: '2020-09-12T20:00:00'
+        },
+        {
+          title: 'Birthday Party',
+          start: '2020-09-13T07:00:00'
+        },
+        {
+          title: 'Click for Google',
+          url: 'http://google.com/',
+          start: '2020-09-28'
+        }
+      ]
+    });
+
+    calendar.render();
+  });
+
+</script>
+</head>
+<body>
