bfaa74f373ef4a2d658ac837e15f5f31f862580f
drewmw5 <66263013+drewmw5@users.noreply.github.com>
2020-11-08 18:26:46 -0800

    updated

--- public/includes/php/SQL/calendar.php
+++ public/includes/php/SQL/calendar.php
@@ -1,23 +1,29 @@
 <?php
+if ($_SERVER['REQUEST_METHOD'] != "POST") {
+    die("Method is not of type POST");
+}
+
 require_once '../../../../dbconn.php';
 
 $return = [];
 
 if($stmt = $con->prepare('SELECT * FROM calendar')) {
     $stmt->execute();
-    $stmt->bind_result($id, $group_type, $title, $url, $start, $end, $all_day);
+    $stmt->bind_result($id, $group_type, $title, $description, $url, $start, $end, $all_day, $classes);
 
     while($stmt->fetch()) {
          array_push($return, 
          
             $result = [
             "id" => $id, 
-            "group_type" =>$group_type, 
-            "title" => $title, 
+            "groupId" =>$group_type, 
+            "title" => $title,
+            "description" => $description,
             "url" => $url, 
             "start" => $start, 
             "end" => $end, 
-            "all_day" => $all_day
+            "allDay" => $all_day,
+            "classNames" => $classes,
             ]
         );
 
--- public/includes/php/cal-head.php
+++ public/includes/php/cal-head.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,10 +9,21 @@
     <script defer src="js/jquery-3.5.1.min.js"></script>
     <script defer src="js/bootstrap.min.js"></script>
     <script defer src="js/calendar.min.js"></script>
+    <script defer src="js/my-calendar.min.js"></script>
     <script defer src="js/index.min.js"></script>
+<!-- 
+
+    <link href='fullcalendar/main.css' rel='stylesheet' />
+    <script src='fullcalendar/main.js'></script>
+    <script>
+        var calendar = new FullCalendar.Calendar(calendarEl, {
+            // no plugin config required!
+        });
+    </script> -->
 
     <!-- Font Awesome JS -->
-    <script defer src="js/all.min.js"></script> <!--load all styles -->
+    <script defer src="js/all.min.js"></script>
+    <!--load all styles -->
 
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/calendar.min.css">
@@ -21,7 +33,8 @@
 
     <link rel="canonical" href=<?php echo $canonical; ?> />
 
-    <title>Apex Tech Racing<?php echo $title;?></title>
+    <title>Apex Tech Racing<?php echo $title; ?></title>
 
 </head>
+
 <body>
