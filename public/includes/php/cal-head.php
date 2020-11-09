44d3abe9b700337670793442ce390b9c5096236e
drewmw5 <66263013+drewmw5@users.noreply.github.com>
2020-11-08 19:14:17 -0800

    seperation of concerns

--- public/includes/php/cal-head.php
+++ public/includes/php/cal-head.php
@@ -13,41 +13,6 @@
     <script defer src="js/index.min.js"></script>
     <script>
 
-document.addEventListener('DOMContentLoaded', function() {
-  var calendarEl = document.getElementById('calendar');
-
-  var calendar = new FullCalendar.Calendar(calendarEl, {
-    headerToolbar: {
-      left: 'prev,next today',
-      center: 'title',
-      right: 'dayGridMonth,listYear'
-    },
-
-    displayEventTime: false, // don't show the time column in list view
-
-    // THIS KEY WON'T WORK IN PRODUCTION!!!
-    // To make your own Google API key, follow the directions here:
-    // http://fullcalendar.io/docs/google_calendar/
-    googleCalendarApiKey: 'AIzaSyAQ43nWBGvG6ZoI88KchAfMBFwK0odQqyE',
-
-    // US Holidays
-    events: {
-    googleCalendarId: 'c_un0ld6k7nuvv85n5q3qevpl3ds@group.calendar.google.com'
-  },
-    eventClick: function(arg) {
-
-      // opens events in a popup window
-      window.open(arg.event.url, '_blank', 'width=700,height=600');
-
-      // prevents current tab from navigating
-      arg.jsEvent.preventDefault();
-    }
-
-  });
-
-  calendar.render();
-});
-
 </script>
 
     <!-- Font Awesome JS -->
--- public/js/my-calendar.js
+++ public/js/my-calendar.js
@@ -1,22 +1,35 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var calendarEl = document.getElementById("calendar");
+document.addEventListener('DOMContentLoaded', function() {
+    var calendarEl = document.getElementById('calendar');
   
     var calendar = new FullCalendar.Calendar(calendarEl, {
       headerToolbar: {
-        left: "prev,next today",
-        center: "title",
-        right: "dayGridMonth,timeGridWeek,timeGridDay",
+        left: 'prev,next today',
+        center: 'title',
+        right: 'dayGridMonth,listYear'
       },
-      navLinks: true, // can click day/week names to navigate views
-      selectable: true,
-      selectMirror: true,
-      editable: true,
-      dayMaxEvents: true, // allow "more" link when too many events
+  
+      displayEventTime: false, // don't show the time column in list view
+  
+      // THIS KEY WON'T WORK IN PRODUCTION!!!
+      // To make your own Google API key, follow the directions here:
+      // http://fullcalendar.io/docs/google_calendar/
+      googleCalendarApiKey: 'AIzaSyAQ43nWBGvG6ZoI88KchAfMBFwK0odQqyE',
+  
+      // US Holidays
       events: {
-          url: 'includes/php/SQL/calendar.php',
-          method: '',
-      },
+      googleCalendarId: 
+      'c_un0ld6k7nuvv85n5q3qevpl3ds@group.calendar.google.com'
+    },
+      eventClick: function(arg) {
+  
+        // opens events in a popup window
+        window.open(arg.event.url, '_blank', 'width=700,height=600');
+  
+        // prevents current tab from navigating
+        arg.jsEvent.preventDefault();
+      }
+  
     });
+  
     calendar.render();
   });
-  
