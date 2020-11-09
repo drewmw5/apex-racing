b3faf1c84e734dbc4ffd759d0001e8dad537d57b
drewmw5 <66263013+drewmw5@users.noreply.github.com>
2020-11-08 19:04:39 -0800

    updated

--- public/includes/php/cal-head.php
+++ public/includes/php/cal-head.php
@@ -9,17 +9,45 @@
     <script defer src="js/jquery-3.5.1.min.js"></script>
     <script defer src="js/bootstrap.min.js"></script>
     <script defer src="js/calendar.min.js"></script>
-    <script defer src="js/my-calendar.min.js"></script>
+    <!-- <script defer src="js/my-calendar.min.js"></script> -->
     <script defer src="js/index.min.js"></script>
-<!-- 
-
-    <link href='fullcalendar/main.css' rel='stylesheet' />
-    <script src='fullcalendar/main.js'></script>
     <script>
-        var calendar = new FullCalendar.Calendar(calendarEl, {
-            // no plugin config required!
-        });
-    </script> -->
+
+document.addEventListener('DOMContentLoaded', function() {
+  var calendarEl = document.getElementById('calendar');
+
+  var calendar = new FullCalendar.Calendar(calendarEl, {
+    headerToolbar: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'dayGridMonth,listYear'
+    },
+
+    displayEventTime: false, // don't show the time column in list view
+
+    // THIS KEY WON'T WORK IN PRODUCTION!!!
+    // To make your own Google API key, follow the directions here:
+    // http://fullcalendar.io/docs/google_calendar/
+    googleCalendarApiKey: 'AIzaSyAQ43nWBGvG6ZoI88KchAfMBFwK0odQqyE',
+
+    // US Holidays
+    events: 'en.usa#holiday@group.v.calendar.google.com',
+
+    eventClick: function(arg) {
+
+      // opens events in a popup window
+      window.open(arg.event.url, '_blank', 'width=700,height=600');
+
+      // prevents current tab from navigating
+      arg.jsEvent.preventDefault();
+    }
+
+  });
+
+  calendar.render();
+});
+
+</script>
 
     <!-- Font Awesome JS -->
     <script defer src="js/all.min.js"></script>
