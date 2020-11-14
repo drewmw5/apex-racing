ae6812e1f5f70774eb6e337a5a527286affe2d74
drewmw5 <66263013+drewmw5@users.noreply.github.com>
2020-11-14 10:20:25 -0800

    added contact pages and links to nav

---
+++ public/contact.php
@@ -0,0 +1,12 @@
+<?php
+$title = " - Contact";
+$desc = '"Welcome to the Apex Tech Racing Contact Page! Here is where you can contact us!"';
+$canonical = '"https://www.apextechracing.com/gallery"';
+
+require_once 'includes/php/cal-head.php';
+require_once 'includes/html/navbar.html';
+require_once 'includes/html/contact.html';
+require_once 'includes/html/footer.html';
+?>
+</body>
+</html>
---
+++ public/includes/php/submit-contact.php
@@ -0,0 +1,10 @@
+<?php
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$content = $_POST['content'];
+
+echo $name . $email . $subject . $content;
+
+?>
